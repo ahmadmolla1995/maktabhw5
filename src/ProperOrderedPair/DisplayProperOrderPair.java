500a7580f43e50461e9155ed661f66a8aa7bb578
ahmadmolla1995 <ahmadmolla.eng93@gmail.com>
2019-12-04 06:01:28 -0800

    proper ordered pair completed

--- src/ProperOrderedPair/DisplayProperOrderPair.java
+++ src/ProperOrderedPair/DisplayProperOrderPair.java
@@ -7,11 +7,18 @@ import java.util.Arrays;
 public class DisplayProperOrderPair {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 7, 9, 2, 5, 5, 8, 5, 6, 3, 4, 7, 3, 1));
-        removeInAppropriateOrderedPairs(numbers);
-        System.out.println(numbers);
+        System.out.println(removeInAppropriateOrderedPairs(numbers));
     }
 
-    private static void removeInAppropriateOrderedPairs(ArrayList<Integer> number) {
-        
+    private static ArrayList<Integer> removeInAppropriateOrderedPairs(ArrayList<Integer> numbers) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < numbers.size() - 1; i += 2) {
+            if (numbers.get(i) <= numbers.get(i + 1)) {
+                result.add(numbers.get(i));
+                result.add(numbers.get(i + 1));
+            }
+        }
+
+        return result;
     }
 }
