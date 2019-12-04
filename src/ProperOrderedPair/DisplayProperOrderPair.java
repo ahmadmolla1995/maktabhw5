fe763484990aa2662d62c032dffdfc07a47ba75f
ahmadmolla1995 <ahmadmolla.eng93@gmail.com>
2019-12-04 04:52:28 -0800

    proper ordered pair folder added

---
+++ src/ProperOrderedPair/DisplayProperOrderPair.java
@@ -0,0 +1,17 @@
+package ProperOrderedPair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class DisplayProperOrderPair {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 7, 9, 2, 5, 5, 8, 5, 6, 3, 4, 7, 3, 1));
+        removeInAppropriateOrderedPairs(numbers);
+        System.out.println(numbers);
+    }
+
+    private static void removeInAppropriateOrderedPairs(ArrayList<Integer> number) {
+        
+    }
+}
